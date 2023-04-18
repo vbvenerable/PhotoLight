5c4d54ee1324fd7deadc3f0390e89bdd893a9058
vbvenerable <131090978+vbvenerable@users.noreply.github.com>
2023-04-18 04:16:58 +0200

    Update header.php

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,32 +1,5 @@
 <?
-/**
- * 
- * PHP versions 3, 4 and 5
- *
- * LICENSE:
- * 
- * This file is part of PhotoLight.
- *
- * PhotoLight is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PhotoLight is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PhotoLight.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @category  Website
- * @package   Photolight
- * @author    Thibaud Rohmer <thibaud.rohmer@gmail.com>
- * @copyright 2011 Thibaud Rohmer
- * @license   http://www.gnu.org/licenses/
- * @link      http://github.com/thibaud-rohmer/PhotoLight
- */
+
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">  
